6d0a55624d5189fa8fde120b82f0bfc494b4f999
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2018-03-05 13:14:14 +0100

    Numerate + copyrights

--- TPOptionsCreator/Assets/TP_Creator/TP_OptionsCreator/Scripts/TPOptionsCreator.cs
+++ TPOptionsCreator/Assets/TP_Creator/TP_OptionsCreator/Scripts/TPOptionsCreator.cs
@@ -1,4 +1,11 @@
-﻿using UnityEngine;
+﻿/**
+*   Authored by Tomasz Piowczyk
+*   Copyright 2018 You're allowed to make changes in functionality and use for commercial or personal.
+*   You're not allowed to claim ownership of this script. 
+*   https://github.com/Prastiwar/TPOptionsCreator
+*   Unity 2017.3.0f
+*/
+using UnityEngine;
 
 namespace TP.Options
 {
